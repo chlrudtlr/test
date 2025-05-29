31ac4e056ac64c2f1d672bd5e862b67df97e79a1
chlrudtlr <misosunboy2@naver.com>
2025-05-29 23:09:39 +0900

    changed by A

--- test.cpp
+++ test.cpp
@@ -6,6 +6,6 @@ int main(){
     cout << "Hello World" << endl;
     cout << "This is main branch" << endl;
 
-    cout << "written by A branch" << endl;
+    cout << "No this is A branch" << endl;
     return 0;
 }
