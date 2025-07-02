f3d1e8c9b2e0f04d045788a387bb130d405bc7f5
최경식 <116335764+chlrudtlr@users.noreply.github.com>
2025-07-03 00:20:22 +0900

    Create practice.cpp

---
+++ practice.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    cout << "Hello World" << endl;
+    
+    cout << "This is main branch" << endl;
+
+    cout << "practice code" <<endl;
+    
+    return 0;
+}
